6c557eae7409d0a2d327d797a8ec7314bc7d7d0d
Guido Günther <agx@sigxcpu.org>
2022-11-26 14:19:23 +0100

    osk-widget: Fix indentation
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- src/pos-osk-widget.h
+++ src/pos-osk-widget.h
@@ -16,17 +16,17 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (PosOskWidget, pos_osk_widget, POS, OSK_WIDGET, GtkDrawingArea)
 
-PosOskWidget      *pos_osk_widget_new (void);
-const char       *pos_osk_widget_get_name (PosOskWidget *self);
-void              pos_osk_widget_set_layer (PosOskWidget *self, PosOskWidgetLayer layer);
-PosOskWidgetLayer pos_osk_widget_get_layer (PosOskWidget *self);
+PosOskWidget     *pos_osk_widget_new (void);
+const char       *pos_osk_widget_get_name   (PosOskWidget *self);
+void              pos_osk_widget_set_layer  (PosOskWidget *self, PosOskWidgetLayer layer);
+PosOskWidgetLayer pos_osk_widget_get_layer  (PosOskWidget *self);
 gboolean          pos_osk_widget_set_layout (PosOskWidget *self,
                                              const char   *display_name,
                                              const char   *layout,
                                              const char   *variant,
                                              GError      **err);
-void              pos_osk_widget_set_mode (PosOskWidget *self, PosOskWidgetMode mode);
-PosOskWidgetMode  pos_osk_widget_get_mode (PosOskWidget *self);
-const char *      pos_osk_widget_get_locale (PosOskWidget *self);
+void              pos_osk_widget_set_mode   (PosOskWidget *self, PosOskWidgetMode mode);
+PosOskWidgetMode  pos_osk_widget_get_mode   (PosOskWidget *self);
+const char       *pos_osk_widget_get_locale (PosOskWidget *self);
 
 G_END_DECLS
