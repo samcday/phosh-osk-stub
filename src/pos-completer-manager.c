e1d608d3b1ac0598a1142f02bc4653234cedb0de
Guido Günther <agx@sigxcpu.org>
2023-04-27 17:01:35 +0200

    completer-manager: Allow to set default completer via GSetting
    Keep presage as the default.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- src/pos-completer-manager.c
+++ src/pos-completer-manager.c
@@ -15,15 +15,18 @@
 # include "completers/pos-completer-fzf.h"
 #endif
 
+#include "contrib/util.h"
+
+#include <gio/gio.h>
+
 /**
  * PosCompleterManager:
  *
  * Maps completion engines to locales.
  *
  * TODO: take user configuration into account.
- *       - map locales to completers
- *       - map keyboard layouts/variants to completers
- *       - allow completers to have different config based on keyboard layout
+ *  - map keyboard variants to default completers
+ *  - allow completers to have different config based on keyboard layout
  */
 
 enum {
@@ -37,8 +40,60 @@ struct _PosCompleterManager {
   GObject           parent;
 
   PosCompleter     *default_;
+  GSettings        *settings;
 };
 G_DEFINE_TYPE (PosCompleterManager, pos_completer_manager, G_TYPE_OBJECT)
+
+
+static PosCompleter *
+init_completer (const char *name, GError **err)
+{
+  if (g_strcmp0 (name, "presage") == 0)
+    return pos_completer_presage_new (err);
+  else if (g_strcmp0 (name, "pipe") == 0)
+    return pos_completer_pipe_new (err);
+#ifdef POS_HAVE_FZF
+  else if (g_strcmp0 (name, "fzf") == 0)
+    return pos_completer_fzf_new (err);
+#endif
+  /* Other optional completer go here */
+  return NULL;
+}
+
+
+static void
+on_default_completer_changed (PosCompleterManager *self)
+{
+  g_autoptr (GError) err = NULL;
+  g_autofree char *default_name = NULL;
+  PosCompleter *default_ = NULL;
+
+  g_assert (POS_IS_COMPLETER_MANAGER (self));
+
+  default_name = g_settings_get_string (self->settings, "default");
+  if (!STR_IS_NULL_OR_EMPTY (default_name)) {
+    default_ = init_completer (default_name, &err);
+    if (default_ == NULL) {
+      g_critical ("Failed to init default completer '%s': %s", default_name,
+                  err ? err->message : "Completer does not exist");
+      g_clear_error (&err);
+    }
+  }
+
+  /* Fallback */
+  if (default_ == NULL)
+    default_ = init_completer (POS_DEFAULT_COMPLETER, &err);
+  if (default_ == NULL) {
+    g_critical ("Failed to init default completer '%s': %s", POS_DEFAULT_COMPLETER,
+                err ? err->message : "Completer does not exist");
+    g_clear_error (&err);
+  }
+
+  if (default_ != NULL) {
+    g_debug ("Switching default completer to '%s'", pos_completer_get_name (default_));
+    g_set_object (&self->default_, default_);
+  }
+}
 
 
 static void
@@ -65,6 +120,7 @@ pos_completer_manager_finalize (GObject *object)
 {
   PosCompleterManager *self = POS_COMPLETER_MANAGER(object);
 
+  g_clear_object (&self->settings);
   g_clear_object (&self->default_);
 
   G_OBJECT_CLASS (pos_completer_manager_parent_class)->finalize (object);
@@ -95,33 +151,37 @@ pos_completer_manager_class_init (PosCompleterManagerClass *klass)
 
 
 static void
+set_initial_completer (PosCompleterManager *self)
+{
+  const char *name = g_getenv ("POS_TEST_COMPLETER");
+  g_autoptr (GError) err = NULL;
+
+  /* Environment */
+  if (name) {
+    self->default_ = init_completer (name, &err);
+    if (self->default_) {
+      g_debug ("Completer '%s' set via environment", pos_completer_get_name (self->default_));
+      return;
+    }
+    g_critical ("Failed to init test completer '%s': %s", name,
+                err ? err->message : "Completer does not exist");
+  }
+
+  /* GSetting */
+  /* Only listen for changes when not using POS_TEST_COMPLETER */
+  g_signal_connect_swapped (self->settings, "changed::default",
+                            G_CALLBACK (on_default_completer_changed),
+                            self);
+  on_default_completer_changed (self);
+}
+
+
+static void
 pos_completer_manager_init (PosCompleterManager *self)
 {
-  g_autoptr (GError) err = NULL;
-  const char *name = g_getenv ("POS_TEST_COMPLETER");
+  self->settings = g_settings_new ("sm.puri.phosh.osk.Completers");
 
-  name = name ?: "presage";
-  if (g_strcmp0 (name, "presage") == 0)
-    self->default_ = pos_completer_presage_new (&err);
-  else if (g_strcmp0 (name, "pipe") == 0)
-    self->default_ = pos_completer_pipe_new (&err);
-#ifdef POS_HAVE_FZF
-  else if (g_strcmp0 (name, "fzf") == 0)
-    self->default_ = pos_completer_fzf_new (&err);
-#endif
-  /* Other optional completer go here */
-
-  if (self->default_ != NULL)
-    return;
-
-  g_critical ("Failed to init '%s' completer: %s", name,
-              err ? err->message : "Completer does not exist");
-
-  /* Fall back to presage */
-  if (g_strcmp0 (name, "presage") != 0) {
-    g_clear_error (&err);
-    self->default_ = pos_completer_presage_new (&err);
-  }
+  set_initial_completer(self);
 }
 
 
